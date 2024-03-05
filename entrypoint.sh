cf4b9f1452c8513f8037ead9d69a1b911a53f598
Oleksii Orel <oorel@redhat.com>
2024-03-05 14:00:16 +0200

    fix: Code refactoring
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+# Ensure $HOME exists when starting
+if [ ! -d "${HOME}" ]; then
+  mkdir -p "${HOME}"
+fi
+
+# Add current (arbitrary) user to /etc/passwd and /etc/group
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-user}:x:$(id -u):0:${USER_NAME:-user} user:${HOME}:/bin/bash" >> /etc/passwd
+    echo "${USER_NAME:-user}:x:$(id -u):" >> /etc/group
+  fi
+fi
+
+exec "$@"
